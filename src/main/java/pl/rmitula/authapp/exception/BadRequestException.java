94c729dba7fb2c5626250cec27f24650d084d602
Dan Mathew <130273326+DanMathew7@users.noreply.github.com>
2023-04-09 12:42:34 +0530

    Update BadRequestException.java
    Co-Authored-By: Alan Binu <alanbinu5@gmail.com>

--- src/main/java/pl/rmitula/authapp/exception/BadRequestException.java
+++ src/main/java/pl/rmitula/authapp/exception/BadRequestException.java
@@ -1,7 +1,7 @@
 package pl.rmitula.authapp.exception;
 
 public class BadRequestException extends RuntimeException {
-
+   //BadRequestException 
     public BadRequestException(String message) {
         super(message);
     }
